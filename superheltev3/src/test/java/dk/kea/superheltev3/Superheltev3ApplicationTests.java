107c0905b6339287f95af1c85b8281ac6e43d85c
Teller501 <ande777j@stud.kea.dk>
2023-02-15 09:55:01 +0100

    added html format for listing all superheroes

---
+++ superheltev3/src/test/java/dk/kea/superheltev3/Superheltev3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.superheltev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Superheltev3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
